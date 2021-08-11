9a2f2557a35be5230daab2648bd3228f3f178ffc
Kritika Sharma <kritikasharma@Kritikas-MacBook-Air.local>
2021-08-12 00:16:18 +0530

    layout done, basic working complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [HorizontalLength, setHorizontalLength] = useState(10)
+  const [VerticalLength, setVerticalLength] = useState(10)
+  const [Blur, setBlur] = useState(10)
+  const [Color, setColor] = useState('black')
   return (
     <div className='App'>
-      <h1>Box shadow generator</h1>
+      <div className='controls'>
+        <label>Horizontal length</label>
+        <input
+          type='range'
+          min='-200'
+          max='200'
+          value={HorizontalLength}
+          onChange={(e) => setHorizontalLength(e.target.value)}
+        />
+
+        <label>Vertical length</label>
+        <input
+          type='range'
+          min='-200'
+          max='200'
+          value={VerticalLength}
+          onChange={(e) => setVerticalLength(e.target.value)}
+        />
+        <label>Blur</label>
+        <input
+          type='range'
+          min='-0'
+          max='200'
+          value={Blur}
+          onChange={(e) => setBlur(e.target.value)}
+        />
+        <label>Color</label>
+        <input
+          type='color'
+          value={Color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <p>
+          box-shadow: {HorizontalLength}px {VerticalLength}px {Blur}px {Color}
+        </p>
+      </div>
+      <div className='output'>
+        <div
+          className='output-box'
+          style={{
+            boxShadow: `${HorizontalLength}px ${VerticalLength}px ${Blur}px ${Color}`,
+          }}
+        ></div>
+      </div>
     </div>
   )
 }
