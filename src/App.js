e4ecbec84c4d7adbddd02946f0331a599dd99281
Kritika Sharma <kritikasharma@Kritikas-MacBook-Air.local>
2021-08-12 11:45:30 +0530

    navabr done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import Navbar from './Navbar'
 
 function App() {
   const [HorizontalLength, setHorizontalLength] = useState(10)
@@ -10,57 +11,60 @@ function App() {
   const [Copied, setCopied] = useState(false)
   const val = `box-shadow: ${HorizontalLength}px ${VerticalLength}px ${Blur}px ${Color};`
   return (
-    <div className='App'>
-      <div className='controls'>
-        <label>Horizontal length</label>
-        <input
-          type='range'
-          min='-200'
-          max='200'
-          value={HorizontalLength}
-          onChange={(e) => setHorizontalLength(e.target.value)}
-        />
+    <>
+      <Navbar />
+      <div className='App'>
+        <div className='controls'>
+          <label>Horizontal length</label>
+          <input
+            type='range'
+            min='-200'
+            max='200'
+            value={HorizontalLength}
+            onChange={(e) => setHorizontalLength(e.target.value)}
+          />
 
-        <label>Vertical length</label>
-        <input
-          type='range'
-          min='-200'
-          max='200'
-          value={VerticalLength}
-          onChange={(e) => setVerticalLength(e.target.value)}
-        />
-        <label>Blur</label>
-        <input
-          type='range'
-          min='-0'
-          max='200'
-          value={Blur}
-          onChange={(e) => setBlur(e.target.value)}
-        />
-        <label>Color</label>
-        <input
-          type='color'
-          value={Color}
-          onChange={(e) => setColor(e.target.value)}
-        />
+          <label>Vertical length</label>
+          <input
+            type='range'
+            min='-200'
+            max='200'
+            value={VerticalLength}
+            onChange={(e) => setVerticalLength(e.target.value)}
+          />
+          <label>Blur</label>
+          <input
+            type='range'
+            min='-0'
+            max='200'
+            value={Blur}
+            onChange={(e) => setBlur(e.target.value)}
+          />
+          <label>Color</label>
+          <input
+            type='color'
+            value={Color}
+            onChange={(e) => setColor(e.target.value)}
+          />
 
-        <p>
-          box-shadow: {HorizontalLength}px {VerticalLength}px {Blur}px {Color}
-        </p>
-        <CopyToClipboard text={val} onCopy={() => setCopied(true)}>
-          <a class='waves-effect waves-light btn-small'>Copy CSS</a>
-        </CopyToClipboard>
-        {/* {Copied ? (Copied = false) : (Copied = true)} */}
+          <p>
+            box-shadow: {HorizontalLength}px {VerticalLength}px {Blur}px {Color}
+          </p>
+          <CopyToClipboard text={val} onCopy={() => setCopied(true)}>
+            <a class='waves-effect waves-light btn-small'>Copy CSS</a>
+          </CopyToClipboard>
+          {/* {Copied ? (Copied = false) : (Copied = true)} */}
+        </div>
+        <div className='output'>
+          <div
+            className='output-box'
+            style={{
+              boxShadow: `${HorizontalLength}px ${VerticalLength}px ${Blur}px ${Color}`,
+            }}
+          ></div>
+        </div>
       </div>
-      <div className='output'>
-        <div
-          className='output-box'
-          style={{
-            boxShadow: `${HorizontalLength}px ${VerticalLength}px ${Blur}px ${Color}`,
-          }}
-        ></div>
-      </div>
-    </div>
+    </>
   )
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div class='nav-wrapper teal lighten-1'>
+          <a href='' class='brand-logo center'>
+            BOX SHADOW GENERATOR
+          </a>
+        </div>
+      </nav>
+    </div>
+  )
+}
